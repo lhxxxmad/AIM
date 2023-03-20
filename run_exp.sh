7e8fd2fe8a32111f34b2a0efbdd1284dd9739b92
taoyang <vectoryyyy@gmail.com>
2023-03-19 21:26:02 -0400

    update config

--- run_exp.sh
+++ run_exp.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Training
-bash tools/dist_train.sh configs/recognition/vit/vitclip_base_diving48.py 4 --test-last --validate \
+bash tools/dist_train.sh configs/recognition/vit/vitclip_base_diving48.py 8 --test-last --validate \
 --cfg-options model.backbone.pretrained=openaiclip work_dir=work_dirs_vit/diving48/debug
 
 # Evaluation only
-# bash tools/dist_test.sh configs/recognition/vit/vitclip_large_k400.py checkpoints/new_vit_L_clip_16frame_k400.pth 4 --eval top_k_accuracy
+# bash tools/dist_test.sh configs/recognition/vit/vitclip_large_k400.py checkpoints/vit_b_clip_8frame_k400.pth 8 --eval top_k_accuracy
